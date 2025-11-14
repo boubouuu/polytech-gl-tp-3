9f7bcaeec1176219d974399b956705f4a44db862
jisele <jisele.jisele@yahoo.fr>
2025-11-14 09:19:42 +0100

    Case10 : fix existingIsOverlapping, implémentation canBook

--- src/main/java/fr/polytech/gl/tp3/Case10BookingService.java
+++ src/main/java/fr/polytech/gl/tp3/Case10BookingService.java
@@ -7,18 +7,23 @@ package fr.polytech.gl.tp3;
  * - Implémenter canBook(currentBookings, newStart, newEnd) :
  *      - retourne false si un créneau chevauche [newStart, newEnd]
  *      - sinon true
- * - Écrire au moins un test unitaire pour canBook.
+ * - Les tests unitaires sont déjà fournis dans le dépôt
  */
 public class Case10BookingService {
 
     // ISSUE : détection de chevauchement incorrecte
     public boolean existingIsOverlapping(int start1, int end1, int start2, int end2) {
-        return start1 == start2 && end1 == end2; // cas trivial seulement
+        // Détection correcte du chevauchement
+        return start1 < end2 && start2 < end1;
     }
 
-    // FEATURE : à implémenter
+    // FEATURE : implémentation correcte
     public boolean canBook(int[][] existingSlots, int newStart, int newEnd) {
-        // TODO: implémenter une vraie détection de chevauchement
-        return false; // volontairement faux
+        for (int[] slot : existingSlots) {
+            if (existingIsOverlapping(slot[0], slot[1], newStart, newEnd)) {
+                return false; // chevauchement trouvé
+            }
+        }
+        return true; // aucun chevauchement
     }
 }
