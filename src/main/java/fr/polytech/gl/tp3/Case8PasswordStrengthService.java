b4e4bad93b0dad0d4df4c5a9f62d1150256df497
Dylan Deshauteurs <dylan.deshauteurs@etu.univ-tours.fr>
2026-09-22 10:04:29 +0200

    fix(case8): optimize character checks and clean code smells

--- src/main/java/fr/polytech/gl/tp3/Case8PasswordStrengthService.java
+++ src/main/java/fr/polytech/gl/tp3/Case8PasswordStrengthService.java
@@ -1,40 +1,37 @@
 package fr.polytech.gl.tp3;
 
-/*
- * Case 8 : évaluation de mot de passe.
- * Tâches :
- * - Corriger la comparaison de chaînes dans existingIsDefaultPassword().
- * - Implémenter score(password) :
- *      - +1 si longueur >= 8
- *      - +1 si contient un chiffre
- *      - +1 si contient une majuscule
- *      - retourne un score entre 0 et 3
- * - Écrire au moins un test unitaire pour score.
- */
-
 public class Case8PasswordStrengthService {
 
-    // ISSUE : comparaison de chaînes avec ==
     public boolean existingIsDefaultPassword(String password) {
-        //return password == "password"; //ancienne version 
-        return "password".equals(password); //version corrigée 
+        return "password".equals(password);
     }
 
-    // FEATURE : à implémenter
     public int score(String password) {
         if (password == null) {
             return 0;
         }
 
         int score = 0;
+        boolean hasDigit = false;
+        boolean hasUpper = false;
 
         if (password.length() >= 8) {
             score++;
         }
-        if (password.matches(".*[0-9].*")) {
+
+        for (char c : password.toCharArray()) {
+            if (Character.isDigit(c)) {
+                hasDigit = true;
+            }
+            if (Character.isUpperCase(c)) {
+                hasUpper = true;
+            }
+        }
+
+        if (hasDigit) {
             score++;
         }
-        if (password.matches(".*[A-Z].*")) {
+        if (hasUpper) {
             score++;
         }
 
