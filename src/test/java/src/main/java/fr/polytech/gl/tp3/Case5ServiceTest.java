68b2b15e92d47db95be69dd2952980673f554da7
Sh3spas <lechanusimon@gmail.com>
2026-09-22 09:42:57 +0200

    feat(case5): implement feature and fix existing issue

--- src/test/java/src/main/java/fr/polytech/gl/tp3/Case5ServiceTest.java
+++ src/test/java/src/main/java/fr/polytech/gl/tp3/Case5ServiceTest.java
@@ -1,14 +1,12 @@
-// test unitaire du case 5
 package fr.polytech.gl.tp3;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Case5ServiceTest {
+class Case5ServiceTest {
 
     @Test
-    public void testToFahrenheit() {
+    void testToFahrenheit() {
         Case5TemperatureConverterService service = new Case5TemperatureConverterService();
         assertEquals(32.0, service.toFahrenheit(0), 0.001);
         assertEquals(212.0, service.toFahrenheit(100), 0.001);
@@ -16,5 +14,12 @@ public class Case5ServiceTest {
         assertEquals(-40.0, service.toFahrenheit(-40), 0.001);
         assertEquals(0.0, service.toFahrenheit(-17.7778), 0.001);
     }
-}
 
+    @Test
+    void testExistingToCelsius() {
+        Case5TemperatureConverterService service = new Case5TemperatureConverterService();
+        assertEquals(0.0, service.existingToCelsius(32.0), 0.001);
+        assertEquals(100.0, service.existingToCelsius(212.0), 0.001);
+        assertEquals(-40.0, service.existingToCelsius(-40.0), 0.001);
+    }
+}
